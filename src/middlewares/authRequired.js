5267b9a55933ceb2997a73ed4dc606591bb3bdb3
Vincent Chavez <vchavez92780@gmail.com>
2017-08-20 21:27:17 -0400

    updated middleware exports

--- src/middlewares/authRequired.js
+++ src/middlewares/authRequired.js
@@ -3,7 +3,7 @@
  */
 import CustomError from '../helpers/CustomError';
 
-export const authRequired = (req, res, next) => {
+export default (req, res, next) => {
   if (!req.isAuthenticated || !req.isAuthenticated()) {
     return next(new CustomError(401, 'Login required.'));
   }
---
+++ src/middlewares/index.js
@@ -0,0 +1,1 @@
+export authRequired from './authRequired';
