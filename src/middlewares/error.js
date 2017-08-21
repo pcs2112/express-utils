307c679bd6605b81da0fd2c7b818ad69041c8247
Vincent Chavez <vchavez92780@gmail.com>
2017-08-20 21:39:25 -0400

    Added middleware to normalize application errors

---
+++ src/middlewares/error.js
@@ -0,0 +1,30 @@
+import CustomError from '../helpers/CustomError';
+
+/**
+ Middleware to handle middleware errors
+ */
+export default (err, req, res, next) => { // eslint-disable-line no-unused-vars
+  if (err) {
+    console.error(err);
+  }
+
+  let status = 500;
+  const error = {};
+
+  if (err.name === 'SequelizeValidationError') {
+    status = 400;
+    const customError = new CustomError(400, 'Validation error.', err);
+    error.message = customError.message;
+    error.errors = customError.errors;
+  } else if (err.name === 'CustomError' && err.errors) {
+    status = 400;
+    error.message = err.message;
+    error.errors = err.errors;
+  } else {
+    status = err.status || 500;
+    error.message = err.message;
+  }
+
+  res.status(status);
+  res.json(error);
+};
--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -3,3 +3,4 @@ export defaultQueryParams from './defaultQueryParams';
 export requiredQueryParams from './requiredQueryParams';
 export normalizeQueryParamsToFloat from './normalizeQueryParamsToFloat';
 export normalizeQueryParamsToInteger from './normalizeQueryParamsToInteger';
+export error from './error';
