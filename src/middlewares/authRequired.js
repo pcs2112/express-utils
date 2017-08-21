4114369c88d4917ce8071a6cfc6512b135c6bbd9
Vincent Chavez <vchavez92780@gmail.com>
2017-08-20 21:03:03 -0400

    transpiled code

--- src/middlewares/authRequired.js
+++ src/middlewares/authRequired.js
@@ -3,7 +3,7 @@
  */
 import CustomError from '../helpers/CustomError';
 
-export default authRequired = (req, res, next) => {
+export const authRequired = (req, res, next) => {
   if (!req.isAuthenticated || !req.isAuthenticated()) {
     return next(new CustomError(401, 'Login required.'));
   }
