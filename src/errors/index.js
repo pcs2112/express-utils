9f366d25f3ed7915182eee1a32ec1c9b5f624893
Vincent Chavez <vchavez92780@gmail.com>
2017-08-20 21:52:15 -0400

    Use common export for all error classes

---
+++ src/errors/index.js
@@ -0,0 +1,6 @@
+export CloudinaryError from './CloudinaryError';
+export CustomError from './CustomError';
+export GoogleMapsError from './GoogleMapsError';
+export SendgridError from './SendgridError';
+export StripeError from './StripeError';
+export TwilioError from './TwilioError';
