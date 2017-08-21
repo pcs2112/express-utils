235ac984436bda2cced5fa48f7176a45456c546a
Vincent Chavez <vchavez92780@gmail.com>
2017-08-20 21:38:18 -0400

    Added middleware to normalize query params to int

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -2,3 +2,4 @@ export authRequired from './authRequired';
 export defaultQueryParams from './defaultQueryParams';
 export requiredQueryParams from './requiredQueryParams';
 export normalizeQueryParamsToFloat from './normalizeQueryParamsToFloat';
+export normalizeQueryParamsToInteger from './normalizeQueryParamsToInteger';
---
+++ src/middlewares/normalizeQueryParamsToInteger.js
@@ -0,0 +1,25 @@
+import { isNumber, normalizeInputToInteger } from 'javascript-utils/lib/number';
+import CustomError from '../helpers/CustomError';
+
+/**
+ * Normalizes the list of query params to integers.
+ *
+ * @param {Array} queryParams
+ * @param {String} type - Allowed types are "query" or "param"
+ */
+export default (queryParams = [], type = 'query') => (req, res, next) => {
+  const invalidParams = [];
+  queryParams.forEach((queryParam) => {
+    if (req[type].hasOwnProperty(queryParam) && isNumber(req[type][queryParam])) {
+      req[type][queryParam] = normalizeInputToInteger(req[type][queryParam]);
+    } else {
+      invalidParams.push(queryParam);
+    }
+  });
+
+  if (invalidParams.length > 0) {
+    return next(new CustomError(400, `"${invalidParams.join(', ')}" must be set to valid numbers.`));
+  }
+
+  return next();
+};
