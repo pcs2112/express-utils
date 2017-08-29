4624ef24af143d9812a61273ffcf91c5451c9e66
Vincent Chavez <vchavez92780@gmail.com>
2017-08-29 01:04:43 -0400

    Added middleware to prevent xss attacks

---
+++ src/middlewares/sanitize.js
@@ -0,0 +1,38 @@
+/**
+ * Middleware to sanitize the body of a request
+ */
+import xssFilters from 'xss-filters';
+
+/**
+ * Recursively applies the xss filter to all t
+ * he string properties of the req objects.
+ * @param {Object} obj
+ */
+const clean = (obj) => {
+  Object.keys(obj).forEach((key) => {
+    if (obj[key] === null) {
+      return;
+    }
+
+    if (typeof obj[key] === 'object') {
+      clean(obj[key]);
+    } else if (typeof req.body[key] === 'string') {
+      obj[key] = xssFilters.inHTMLData(obj[key].trim());
+    }
+  });
+};
+
+export default (types) => (req, res, next) => {
+  let normalizedTypes;
+  if (typeof types === 'string') {
+    normalizedTypes = types.split(',').map((type) => type.trim());
+  }
+
+  ['body', 'query', 'params'].forEach((type) => {
+    if (normalizedTypes.indexOf(type) > -1 && req[type]) {
+      clean(req[type]);
+    }
+  });
+
+  next();
+};
